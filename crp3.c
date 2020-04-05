6082d1b50a40bf9affaec3fc38a40dc4b04e90ed
zrthstr <zrth1k@gmail.com.com>
2020-04-05 17:24:45 +0200

    add rate etc

--- crp3.c
+++ crp3.c
@@ -16,22 +16,39 @@
 #include <stdlib.h>
 #include "crplib.c"
 
+int sbx(int len, char key, char * a, char * out){
+	int i = 0;
+	for (;i<len;i++){
+		out[i] = a[i] ^ key;
+	}
+	return i;
+}
 
 int main(){
 	char * hexin = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 	printf("hexin= %si\n", hexin);
-	size_t size_hexin =sizeof(hexin[0]) / sizeof(char);
+	//size_t size_hexin =sizeof(hexin[0]) / sizeof(char);
+	size_t size_hexin = 40 ; //sizeof(hexin[0]) / sizeof(char);
 	char *in = malloc(size_hexin);
+	char *out = malloc(size_hexin);
+	printf("::xx::%ld\n",size_hexin);
 	hstrtob(size_hexin,hexin,in);
-	my_pprint(size_hexin/2,"in:",in);
 
-	int space = 255;
-	int c = 0;
+	//my_pprint(size_hexin/2,"in:",in);
+
+	int space = 128; // unsure if 128 or 256
+	//int space = 256; // unsure if 128 or 256
+	int c = -120;
+	int rate = 0;
 
 	for (;c<=space;c++){
-		printf("c:%d",c);
-		//sbx(c,in,out);
-	
+		//printf("[c(%d)]\n",c);
+		sbx(size_hexin,c,in,out);
+		//printf("::%s",out);
+		//my_pprint(size_hexin/2," [0]",out);
+		//my_pprint2(size_hexin/2," [2]",out);
+		rate = human_rate(size_hexin, out);
+		printf("rate: %d", rate);
+		my_pprint3(size_hexin," [2]",out);
 	}
-
 }
--- crplib.c
+++ crplib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int hchar_val(char i){
 	if (i >= '0' && i <= '9'){return i - '0';}
@@ -11,12 +12,44 @@ int hstrtob (int n, char *hstr, char *to){
 	int c = 0;
 	//char this = 0;
 	for (;i<n;i++){
-		char this = hchar_val( hstr[i]) * 16 ;
+		char this;
+		this = hchar_val( hstr[i]) * 16 ;
 		++i;
 		to[c] = this + hchar_val(hstr[i]);
 		++c;
 	}
 	return c;
+}
+
+
+int human_rate_c(char c){
+	//printf("looking at %c",c);
+	switch (c){
+		case ' ':
+			return 1200;
+		case 'e':
+			return  800;
+		case 'a':
+			return 700;
+		case 't':
+			return 600;
+		case 'o':
+			return 500;
+		case 'n':
+			return 450;
+		case 'm':
+			return 430;
+		default :
+			return 100;
+	}
+}
+
+int human_rate(int len, char * str){
+	int score = 0;
+	for (int i=0; i<len; i++)
+		score = score + human_rate_c(str[i]);
+	printf("[SCORING] Found:%d",score);
+	return score / len;
 }
 
 int my_pprint(int len, char *head, char *str){
@@ -29,4 +62,46 @@ int my_pprint(int len, char *head, char *str){
 	}
 	printf("\n");
 	return i;
+}
+
+int my_pprint2(int len, char *head, char *str){
+	printf("%s:",head);
+	int c; c = 0;
+	for (; c<len; c++){
+		char this; this = str[c];
+		if(isprint(this))
+			printf("%c",this);
+		else
+			switch (this){
+			case '\n':
+				printf("\\n");
+				break;
+			default:
+				printf("\\x%02x", this);
+				break;
+		}
+	}
+	printf("\n");
+	return c;
+}
+
+int my_pprint3(int len, char *head, char *str){
+	printf("%s:",head);
+	int c; c = 0;
+	for (; c<len; c++){
+		char this; this = str[c];
+		if(isprint(this))
+			printf("%c",this);
+		else
+			switch (this){
+			case '\n':
+				printf("\\n");
+				break;
+			default:
+				printf(".");
+				break;
+		}
+	}
+	printf("\n");
+	return c;
 }
